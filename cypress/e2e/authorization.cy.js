1f37c3f65d3605a9031273e21c9a816087488666
Marina Humeniuk <gymenuk93@gmail.com>
2022-11-06 20:54:42 +0200

    commit

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -10,6 +10,5 @@ import user from '../fixtures/user.json'
  it('Verify login helper', {retries: 2}, () => {
    authorizationPage.visit();
    authorizationPage.submitLoginForm(user.email, user.password);
-   fddf
  })
  
